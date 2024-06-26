69a18af2bd411116ae3627c055eb20076f182efa
Madelin FG <84932921+MadelinFg@users.noreply.github.com>
2024-06-25 18:16:44 -0600

    Add complete tasks functionality, Update Home sytles and structure, Update Button styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,63 +16,108 @@ function Home({}) {
     };
 
     const handleAddTask = () => {
-        setPendingTasks([...pendingTasks, task]);
+        if (task === "") {
+            alert("El campo de tarea no puede quedar vacío");
+        } else {
+            setPendingTasks([task, ...pendingTasks]);
 
-        clearForm();
+            clearForm();
+        }
     };
 
-    const handleCompleteTask = () => {};
+    const handleCompleteTask = (index) => {
+        let taskToComplete = pendingTasks[index];
+        setCompletedTasks([taskToComplete, ...completedTasks]);
 
-    const handleDeleteTask = (index) => {
-        const temp = [...pendingTasks];
-        temp.splice(index, 1);
-        setPendingTasks(temp);
+        handleDeleteTask(index, 1);
+    };
+
+    const handleDeleteTask = (index, taskList) => {
+        let temp = [];
+
+        if (taskList === 1) {
+            temp = [...pendingTasks];
+            temp.splice(index, 1);
+
+            setPendingTasks(temp);
+        } else {
+            temp = [...completedTasks];
+            temp.splice(index, 1);
+
+            setCompletedTasks(temp);
+        }
     };
 
     return (
         <div className="home">
             <h1 className="title">Lista de tareas</h1>
 
-            <div className="form-container">
+            <form className="form-container">
                 <Input
                     type="text"
                     title="Escribe una nueva tarea"
                     name="inpt-task"
                     value={task}
                     onChange={(e) => setTask(e.target.value)}
-                    required
                 />
 
-                <Button text="Agregar" onClick={handleAddTask} />
-            </div>
+                <Button type="button" text="Agregar" onClick={handleAddTask} />
+            </form>
 
-            <ul className="in-process-container">
+            <ul className="tasks-container">
                 <h2 className="title-task-container">Tareas pendientes</h2>
 
-                {
-                    pendingTasks.length > 0 ? 
-
+                {pendingTasks.length > 0 ? (
                     pendingTasks.map((task, index) => {
                         return (
                             <li key={index} className="task-element">
                                 <p>{task}</p>
-    
+
                                 <div className="actions-container">
                                     <Button
                                         text="Completar"
-                                        onClick={() => handleCompleteTask(index)}
+                                        onClick={() =>
+                                            handleCompleteTask(index)
+                                        }
                                     />
                                     <Button
                                         text="Eliminar"
-                                        onClick={() => handleDeleteTask(index)}
+                                        onClick={() =>
+                                            handleDeleteTask(index, 1)
+                                        }
                                     />
                                 </div>
                             </li>
                         );
                     })
-                    :
+                ) : (
                     <p>Aún no tienes tareas pendientes</p>
-                }
+                )}
+            </ul>
+
+            <ul className="tasks-container">
+                <h2 className="title-task-container">Tareas completadas</h2>
+
+                {completedTasks.length > 0 ? (
+                    completedTasks.map((task, index) => {
+                        return (
+                            <li key={index} className="task-element">
+                                <p className="task-txt-completed">{task}</p>
+
+                                <div className="actions-container">
+                                    <Button
+                                        text="Eliminar"
+                                        onClick={() =>
+                                            handleDeleteTask(index, 2)
+                                        }
+                                    />
+                                </div>
+                            </li>
+                        );
+                    })
+                ) : (
+                    <p>Aún no tienes tareas completadas</p>
+                )}
             </ul>
         </div>
     );
